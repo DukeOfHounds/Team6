7fb15abf9912000206d3fc9460c8982c3c8ea598
Luke Perry <46532702+PerryProjects@users.noreply.github.com>
2022-01-07 15:43:05 -0500

    check for ghosts

--- Projects/P1/src/PacMan.java
+++ Projects/P1/src/PacMan.java
@@ -4,6 +4,7 @@ import javax.swing.JComponent;
 import java.util.Random;
 
 public class PacMan{
+
 	String myName;
 	Location myLoc;
 	Map myMap;
@@ -24,9 +25,24 @@ public class PacMan{
 		if (moves.isEmpty()) {
 			return false;
 		}
+
+		// get random move
 		Random rand = new Random();
 		Location move = moves.get(rand.nextInt(moves.size()));
-		return myMap.move(myName, move, Map.Type.PACMAN);
+
+		// loc contains null or wall
+		if (!myMap.move(myName, move, Map.Type.PACMAN)) {
+			return false;
+		}
+
+		// location contains ghost
+		if (myMap.getLoc(move).contains(Map.Type.GHOST)) {
+			return false;
+		}
+
+		// complete move
+		this.myLoc = move;
+		return true;
 	}
 
 	public boolean is_ghost_in_range() { 
