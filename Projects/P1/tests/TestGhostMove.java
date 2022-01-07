b9e7f0792bb93dc472ab6860f2b8472cd29b255f
Luke Perry <46532702+PerryProjects@users.noreply.github.com>
2022-01-07 17:08:48 -0500

    init test

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -3,8 +3,25 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestGhostMove extends TestCase {
+	protected NoFrame frame;
+	protected void setUp()  throws FileNotFoundException {
+		//Creating A Map
+		frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
+		//Creating Players
+		Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); 
+		PacMan pacman = frame.addPacMan(new Location(0, 0)); 
+
+		//Start The Game
+		frame.startGame();
+	}
 
 	public void testGhostMove() throws FileNotFoundException{
-	
+		//Creating A Map
+		// Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+		// NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+		assertTrue("hello world".equals("hello world"));
+		// assertTrue(fValue1 == 2.0);
+		// assertTrue("hello world".equals("hello world"));	
+		// assertTrue("hello world".equals("hello orld"));	
 	}
 }
