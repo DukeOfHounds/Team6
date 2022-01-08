2d421a48a1b565b59ea86948ae5081e76af8aeaa
natiwb <nathanbekele120@gmail.com>
2022-01-08 18:01:14 -0500

    Test for Pacman ghostInRange()

--- Projects/P1/tests/TestGhostInRange.java
+++ Projects/P1/tests/TestGhostInRange.java
@@ -3,9 +3,18 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestGhostInRange extends TestCase {
-
-	public void testGhostInRange() throws FileNotFoundException{
-		return null;
-	}
+    protected NoFrame frame;
+    public PacMan pacman;
+    public Ghost g1,g2;
+    protected void setUp()  throws FileNotFoundException {
+        //Creating A Map
+        frame = new NoFrame(); 
+        pacman = frame.addPacMan(new Location(2, 2)); 
+        g1 = frame.addGhost(new Location(2,3),"Biren", Color.red);
+    }
+    public void testGhostInRange()
+    {
+        assertTrue(pacman.is_ghost_in_range());
+    }
 }
 
