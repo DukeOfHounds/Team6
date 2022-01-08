9de81c3f4f34186b42dd9350cb06007e1fdc9653
Luke Perry <46532702+PerryProjects@users.noreply.github.com>
2022-01-07 19:11:42 -0500

    add tests for ghost move

--- Projects/P1/tests/TestGhostMove.java
+++ Projects/P1/tests/TestGhostMove.java
@@ -1,27 +1,27 @@
 import junit.framework.*;
 import java.awt.Color;
 import java.io.*;
+import java.util.ArrayList;
 
 public class TestGhostMove extends TestCase {
 	protected NoFrame frame;
+	public Ghost ghost;
 	protected void setUp()  throws FileNotFoundException {
 		//Creating A Map
-		frame = new NoFrame(); //Creates A New Map With Walls and Tokens Initialized
-		//Creating Players
-		Ghost ghost = frame.addGhost(new Location(1, 1), "name", Color.red); 
-		PacMan pacman = frame.addPacMan(new Location(0, 0)); 
-
+		frame = new NoFrame(); 
+		// make sure ghost's only valid move is ??,??
+		ghost = new Ghost("ghost", new Location(1, 1), frame.getMap());
+		frame.addGhost(new Location(1, 1), "ghost", Color.blue);
 		//Start The Game
 		frame.startGame();
 	}
 
 	public void testGhostMove() throws FileNotFoundException{
-		//Creating A Map
-		// Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
-		// NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
-		assertTrue("hello world".equals("hello world"));
-		// assertTrue(fValue1 == 2.0);
-		// assertTrue("hello world".equals("hello world"));	
-		// assertTrue("hello world".equals("hello orld"));	
+		// confirm ghost location is 1,1
+		assertTrue(frame.getMap().getLoc(new Location(1,1)).contains(Map.Type.GHOST));
+		// move ghost, only valid moves are 2,1 or 1,2 because of walls
+		ghost.move();
+		// confirm ghost is not at 1,1 anymore
+		assertTrue(!frame.getMap().getLoc(new Location(1,1)).contains(Map.Type.PACMAN));
 	}
 }
